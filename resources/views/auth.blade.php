9a152e8aa531fb8a98eeb4823bc4ccfe8b391da6
Ecoleplus <743038-ecoleplus@users.noreply.gitlab.com>
2024-09-01 13:53:03 +0100

    fixes

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,24 +8,28 @@
 
     <title>{{ config('app.name') }} ― Canvas</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css', 'vendor/canvas') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'], 'vendor/amsaid/canvas')
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="stylesheet" href="//fonts.googleapis.com/css2?family=Karla&family=Caveat:wght@700&display=swap">
 
     <style>
-        body,html{
-            height:100%;
+        body,
+        html {
+            height: 100%;
         }
+
         .font-cursive {
             font-family: 'Caveat', cursive;
         }
     </style>
 </head>
+
 <body>
-<div class="container h-100">
-    <div class="row h-100 align-items-center justify-content-center">
-        @yield('content')
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center">
+            @yield('content')
+        </div>
     </div>
-</div>
 </body>
+
 </html>
